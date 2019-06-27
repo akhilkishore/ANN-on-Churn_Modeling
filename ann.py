68b91f1a6c151aa4e4da3d32a7f07cfc5da95e65
Akhil Kishore <akhilkishorek13@gmail.com.com>
2019-06-27 23:27:57 +0530

    updated till data encoding

---
+++ ann.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+#encode categorical datas
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X_1 = LabelEncoder()
+X[:,1] = labelencoder_X_1.fit_transform(X[:,1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:,2] = labelencoder_X_2.fit_transform(X[:,2])
+
+print(X)
